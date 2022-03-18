585a93a25c184b25b6b479092b505eabeeca62af
lushan1980 <shanswork@yahoo.com>
2022-03-18 15:17:13 -0400

    Procfile

--- MyWeb/settings.py
+++ MyWeb/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -37,10 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-   
+    'whitenoise.runserver_nostatic',   
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,10 @@
+web: waitress-serve \
+--listen "*:$PORT" \
+--trusted-proxy '*' \
+--trusted-proxy-headers 'x-forwarded-for x-forwarded-proto x-forwarded-port' \
+--log-untrusted-proxy-headers \
+--clear-untrusted-proxy-headers \
+--threads ${WEB_CONCURRENCY:-4} \
+MyWeb:wsgifunc
+
+web: python manage.py runserver
